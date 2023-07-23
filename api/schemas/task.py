88368ac1d6ee1d9c28a827d17fbc7f49abc24e07
ryota-09 <n.ryo950911@gmail.com>
2023-07-24 08:29:25 +0900

    routerの更新

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,7 +1,20 @@
 from pydantic import BaseModel, Field
+  
+class TaskBase(BaseModel):
+  title: str | None = Field(None, example="クリーニングを取りに行く")
+  
+class TaskCreate(TaskBase):
+  pass
 
-class Task(BaseModel):
+class TaskCreateResponse(TaskCreate):
   id: int
-  title: str | None = Field(None, examples="クリーニングを取りに行く")
+  
+  class Config:
+    orm_mode = True
+
+class Task(TaskBase):
+  id: int
   done: bool = Field(False, description="完了フラグ")
   
+  class Config:
+    orm_mode = True
