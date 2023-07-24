a33d91d3f4624687ef4d1e9f9362f699b1dd4e0f
ryota-09 <n.ryo950911@gmail.com>
2023-07-24 21:23:15 +0900

    modelの作成

--- api/db.py
+++ api/db.py
@@ -6,6 +6,8 @@ DB_URL = "mysql+pymysql://root@db:3306/demo?charaset=utf8"
 db_engine = create_engine(DB_URL, echo=True)
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
+Base = declarative_base()
+
 def get_db():
   with db_session() as session:
     yield session
