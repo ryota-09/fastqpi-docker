ab64f4b7d2e690d264b10cdd65c9c3627acc9eda
ryota-09 <n.ryo950911@gmail.com>
2023-07-26 08:43:29 +0900

    getlistメソッドの動作確認

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,11 +1,22 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.orm import Session
 import api.models.task as task_model
 import api.schemas.task as task_schema
 
 def create_task(db: Session, task_create: task_schema.TaskCreate) -> task_model.Task:
   task = task_model.Task(**task_create.model_dump())
-  print(f"@@@@@@@@@@@@@ {task}")
   db.add(task)
   db.commit()
   db.refresh(task)
   return task
+
+def get_tasks_with_done(db: Session) -> task_model.Task:
+  result: Result = db.execute(
+    select(
+      task_model.Task.id,
+      task_model.Task.title,
+      task_model.Done.id.isnot(None).label("done"),
+    ).outerjoin(task_model.Done)
+  )
+  return result.all()
--- api/routers/task.py
+++ api/routers/task.py
@@ -7,8 +7,8 @@ from api.db import get_db
 router = APIRouter()
 
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks():
-  return [task_schema.Task(id=1, title="1つ目のtodoタスク")]
+async def list_tasks(db: Session = Depends(get_db)):
+  return task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreate)
 async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
